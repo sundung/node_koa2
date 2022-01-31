15e5277b555b446db37de667a1ce5e28ab13448c
“sundung” <sun1358742@163.com>
2022-01-31 20:52:48 +0800

    路由分层

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ const Koa = require('koa');
 const app = new Koa();
 
 const {APP_PORT} = require('./config')
+const useRouter = require('./router/users')
 
-app.use((ctx,next)=>{
-    ctx.body = '你好111'
-})
+app.use(useRouter.routes())
 
 app.listen(APP_PORT,()=>{
     console.log(`启动成功,端口 ${APP_PORT}`)
