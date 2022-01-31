d60397712776baa36e12e8c48acd76c5be772388
“sundung” <sun1358742@163.com>
2022-01-31 21:02:42 +0800

    拆分app业务层

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
-const Koa = require('koa');
-
-const app = new Koa();
-
 const {APP_PORT} = require('./config')
-const useRouter = require('./router/users')
 
-app.use(useRouter.routes())
+const app = require('./app')
 
 app.listen(APP_PORT,()=>{
     console.log(`启动成功,端口 ${APP_PORT}`)
